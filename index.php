5f046be197e66d5cc4f664686a126a772b309aed
Oscar Alencar <oscar.alencar@gmail.com>
2017-01-04 11:04:48 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,229 @@
+<!-- <META http-equiv="refresh" content="1;URL=http://teste.draericacavalcante.com.br"> -->
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="Dra. Erica Cavalcante - Consultório de Reumatologia Pediátrica - Médica especialista em reumatologia pediátrica. ">
+	<meta name="author" content="Oscar Alencar">
+	<meta name="keywords" content="reumatologia pediátrica, reumatologia pediatrica, reumatologia pediátrica belém, reumatologia pediatrica belém, reumatologia clínica, clinica pediatrica, clínica pediatrica, clínica pediátrica ">
+	<title>Dra. Erica Cavalcante - Consultório de Reumatologia Pediátrica</title>
+	<link rel="shortcut icon" href="">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/style.css">
+
+	<link href='http://fonts.googleapis.com/css?family=Lato:300,300italic|Droid+Serif:700' rel='stylesheet' type='text/css'>
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-8230104-20', 'auto');
+		ga('send', 'pageview');
+
+
+	</script>
+	<script type="text/javascript">
+		ga(function(tracker){
+			ref = tracker.get('referrer');
+			console.log(ref);
+			if (ref.indexOf("alencar") >= 0) {
+				tracker.set('referrer', 'http://example.com');
+				console.log(tracker.get('referrer'));
+			}
+			// if (ref === 'http://oscaralencar.com.br/') {
+			// 	tracker.set('referrer', 'http://example.com');
+			// 	console.log(tracker.get('referrer'));
+			// }
+
+		})
+	</script>
+
+	<!--[if IE]>
+								<script src="https://cdn.jsdelivr.net/html5shiv/3.7.2/html5shiv.min.js"></script>
+								<script src="https://cdn.jsdelivr.net/respond/1.4.2/respond.min.js"></script>
+				<![endif]-->
+	<script type="text/javascript">
+		// Tratamento para requisições de dispositivos mobile
+(function(a) {
+    (jQuery.browser = jQuery.browser || {}).mobile = /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od|ad)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))
+})(navigator.userAgent || navigator.vendor || window.opera);
+
+
+if (jQuery.browser.mobile || navigator.userAgent.toLowerCase().indexOf('tablet') > - 1 || navigator.userAgent.toLowerCase().indexOf('android') > - 1 || navigator.userAgent.toLowerCase().indexOf('kfapwi') > - 1) {
+
+    var link = $(location).attr('href');
+    //link = link.toLowerCase();
+
+    /*var x = link.split('://');
+            var l = x[1].indexOf('www.');*/
+
+    link = link.replace('www.', 'teste.');
+    link = link.replace('WWW.', 'teste.');
+    link = link.replace('http://draericacavalcante.com.br', 'http://teste.draericacavalcante.com.br');
+
+    $(location).attr("href", link);
+}
+
+
+	</script>
+</head>
+
+<body>
+
+<?
+
+//	if ($_GET['mensagem'] == 'enviada')
+//	{
+//		echo '<div class="alert alert-success">Sua mensagem foi enviada com sucesso! Aguarde que entraremos em contato brevemente. Obrigado pelo contato.</div>';
+//	} elseif ($_GET['mensagem'] == 'erro')
+//	{
+//		echo '<div class="alert alert-danger">Houve um erro no envio. Tente novamente ou ligue para nós relatanto o problema. Desculpe-nos o transtorno.</div>';
+//} else {
+//	echo "";
+//	}
+?>
+
+	<section id="top">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="top-site">
+						<h2>Dra. Erica Cavalcante</h2>
+						<h1>Consultório de Reumatologia Pediátrica</h1>
+						<p class="lead">Médica especialista em reumatologia pediátrica. Há anos atendendo nossas crianças e adolescentes com amor e dedicação.</p>
+					</div>
+				</div>
+			</div>
+		</div><!--/container-->
+	</section>
+		<div class="container">
+		<div class="row">
+			<div class="col-xs-12 col-sm-6 col-md-4 col-md-push-4 services services-center">
+				<h3>Tratamentos Contra</h3>
+				<ul>
+					<li>Lupus Eritematoso Sistêmico Juvenil</li>
+					<li>Artrite Idiopática Juvenil</li>
+					<li>Esclerodermia </li>
+					<li>Dermatomiosite Juvenil</li>
+					<li>Púrpura de Henoch-Schonlein</li>
+					<li>Doença de Kawasaki</li>
+					<li>Síndromes Auto-Inflamatórias</li>
+					<li>Dentre outras</li>
+				</ul>
+			</div>
+
+			<div class="col-xs-12 col-sm-6 col-md-4 col-md-pull-4 services ">
+			<h3>Clínica Exclusiva</h3>
+<p>Trate seu filho em uma das únicas clínicas em Belém e no Estado do Pará especializada em doenças reumatológicas pediátricas.</p>
+			<iframe src="https://www.google.com/maps/embed?pb=!1m23!1m12!1m3!1d63816.955087672846!2d-48.52299872619077!3d-1.4391997417235391!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m8!3e6!4m0!4m5!1s0x92a48ebb2c84adbf%3A0x8d7325b930f594e3!2sAv.+Sen.+Lemos%2C+443%2C+Bel%C3%A9m+-+PA%2C+66050-000!3m2!1d-1.4392!2d-48.4886658!5e0!3m2!1spt-BR!2sbr!4v1439807313745" width="100%" height="150" frameborder="0" style="border:0" allowfullscreen></iframe>
+
+			</div>
+			<div class="col-xs-12 col-md-4 services services-right">
+				<h4>Diagnósticos Precisos</h4>
+				<p>O primeiro passo para um tratamento correto e eficaz das doenças reumatológicas.</p>
+				<hr>
+				<h4>Acompanhamento Eficaz</h4>
+				<p>Acompanhamos com afinco a evolução do tratamento do paciente.</p>
+			</div>
+
+		</div><!--/row-->
+		<section id="perfil">
+		<div class="row">
+			<div class="col-xs-12">
+
+				<img src="img/dra-erica.jpg" alt="Foto de Dra. Erica Cavalcante - Pediatra" class="img-circle img-responsive pull-left foto-perfil">
+				<h3>Dra. Erica Gomes Cavalcante</h3>
+				<p><em>CRM 8966</em></p>
+				<p>Formada pela Universidade Estadual do Pará, se especializou em pediatria pela Santa Casa de Misericordia do Pará antes de concluir sua pós-graduação em Reumatologia Pediátrica na FMUSP-SP. É coordenadora da residência médica de pediatria da FDCMPA e professora do curso de medicina da UEPA.</p>
+			</div>
+		</div><!--/row-->
+		</section>
+	</div><!--/container-->
+	<section id="contato">
+	<div class="container">
+			<div class="row">
+				<div class="col-xs-12">
+					<h2 class="contato-tit">Entre em Contato</h2>
+					<div class="wrap-contato-form">
+
+  <form action="processa-form.php" method="post" id="formularioContato">
+ 	<div class="form-group">
+ 		<input type="text" class="form-control" placeholder="Nome" name="nome">
+ 	</div>
+ 	<div class="form-group">
+ 		<input type="text" class="form-control" placeholder="Email" name="email">
+ 	</div>
+ 	<div class="form-group">
+ 		<input type="text" class="form-control" placeholder="Telefone" name="telefone">
+ 	</div>
+ 	<div class="form-group">
+ 		<textarea class="form-control" placeholder="Escreva sua mensagem" name="mensagem"></textarea>
+ 	</div>
+	  <input type="submit" class="btn btn-info btn-block" value="Enviar Mensagem"></input>
+  </form>
+
+
+
+					</div><!--/wrap-contato-form-->
+				</div>
+			</div><!--/row-->
+		</div><!--/container-->
+		</section>
+	<div class="container">
+		<footer>
+			<div class="row">
+				<div class="col-xs-12 col-md-4">
+					<address>
+						<div>Consultório Dra. Erica Cavalcante</div>
+						<div>End. Av senador Lemos, 443. Ed Village Executive, Sala 908/909.</div>
+						<div>Tel. (91) 3241 7905 / 3230 4633</div>
+					</address>
+				</div>
+				<div class="col-xs-12 col-md-4"><p>© Dra. Erica Gomes Cavalcante - 2015 Todos os Direitos Reservados</p></div>
+				<div class="col-xs-12 col-md-4">Desenvolvido por <a href="http://www.oscaralencar.com.br">Oscar Alencar</a></div>
+			</div>
+		</footer>
+	</div><!-- /container-->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="js/jqueryvalidate/jquery.validate.js" type="text/javascript"></script>
+	<script type="text/javascript">
+	$(document).ready( function() {
+		  $("#formularioContato").validate({
+			// Define as regras
+			rules:{
+			  nome:{
+				// campoNome será obrigatório (required) e terá tamanho mínimo (minLength)
+				required: true, minlength: 2
+			  },
+			  email:{
+				// campoEmail será obrigatório (required) e precisará ser um e-mail válido (email)
+				required: true, email: true
+			  },
+			  mensagem:{
+				// campoMensagem será obrigatório (required) e terá tamanho mínimo (minLength)
+				required: true, minlength: 2
+			  }
+			},
+			// Define as mensagens de erro para cada regra
+			messages:{
+			  nome:{
+				required: "Digite o seu nome",
+				minLength: "O seu nome deve conter, no mínimo, 2 caracteres"
+			  },
+			  email:{
+				required: "Digite o seu e-mail para contato",
+				email: "Digite um e-mail válido"
+			  },
+			  mensagem:{
+				required: "Digite a sua mensagem",
+				minLength: "A sua mensagem deve conter, no mínimo, 2 caracteres"
+			  }
+			}
+		  });
+		});
+	</script>
+
+
+</body>
+
+</html>
